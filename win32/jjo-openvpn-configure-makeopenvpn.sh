018d84de679c4192346eb910be83024eadbac5f8
JuanJo Ciarlante <juanjosec@gmail.com>
2011-05-10 23:38:53 +0200

    win32: +XAUTOCONF=--enable-password-save

--- win32/jjo-openvpn-configure-makeopenvpn.sh
+++ win32/jjo-openvpn-configure-makeopenvpn.sh
@@ -1,4 +1,5 @@
-rm Makefile config.status config.h
+rm -f Makefile config.status config.h openvpn
+XAUTOCONF=--enable-password-save \
 OPENSSL_DIR=$PWD/../win32/openssl-0.9.8r \
 LZO_DIR=$PWD/../win32/lzo-2.04 \
 PKCS11_HELPER_DIR=$PWD/../win32/pkcs11-helper-1.08 \
